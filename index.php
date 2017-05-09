b89710c04be2b915da56b00d8a0c2a0f4bd057d1
spigatl <spigatl@gmail.com>
2017-05-08 21:31:39 -0400

    v0.6

--- index.php
+++ index.php
@@ -8,179 +8,193 @@
 	<link rel="stylesheet" type="text/css" href="css/site.css">
 </head>
 <body>
-<div class="container">
-	<div class="row" id="logo">
-		<nav id="nav-color" class="navbar navbar-default">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<a class="navbar-brand" href="#"><img src="img/Logo.png" alt="" id="imglogo"/></a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Home</a></li>
-						<li id="menu-link-dropdown" class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Painel <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Area Restrita</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Institucional <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Sobre</a></li>
-								<li><a href="#">Coordenadores</a></li>
-								<li><a href="#">Integrantes</a></li>
-								<li><a href="#">Atividades</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Fotos</a></li>
-						<li><a href="#">Agenda</a></li>
-						<li><a href="#">Contato</a></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container-fluid -->
-		</nav>
-		
-	</div>
-	<div class="row" id="corpo">
-		<div class="col-xs-9">
-			<div class="row">
-				<div class="col-xs-12">
-					<div id="carousel-pam" class="carousel slide" data-ride="carousel">
-						<!-- Indicators -->
-						<ol class="carousel-indicators">
-							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-						</ol>
-
-						<!-- Wrapper for slides -->
-						<div class="carousel-inner" role="listbox">
-							<div class="item active">
-								<div class="col-xs-6 col-xs-offset-1">
-									<h1 style="color: black;">
-										PAM – 23 Anos
-										<br/>
-									</h1>
-									<h3 style="color:#ebbc11">
-										“Sinergia e Sustentabilidade no Alto Tietê”.
-									</h3>
-								</div>
-								<div class="col-xs-5">
-									<img src="img/img1.4.png" alt="...">
-								</div>
-							</div>
-							<div class="item">
-								<div class="col-xs-6 col-xs-offset-1">
-									<h1 style="color: black;">Excelência
-										<br/>
-									</h1>
-									<h3 style="color:#ebbc11">
-										Nossos treinamentos são ministrados e certificados por órgãos competentes, sob coordenação Operacional do 17º GB. 
-									</h3>
-								</div>
-								<div class="col-xs-5">
-									<img src="img/img2.1.png" alt="...">
-								</div>
-							</div>
-						</div>
-
-						<!-- Controls -->
-						<a class="left carousel-control" href="#carousel-pam" role="button" data-slide="prev">
-							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#carousel-pam" role="button" data-slide="next">
-							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
+	<div class="container">
+		<div class="row" id="logo">
+			<nav id="nav-color" class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#"><img src="img/Logo.png" alt="" id="imglogo"/></a>
 					</div>
-				</div>
-				<div class="col-xs-12">
-					<a href="#">
-						<div class="panel ">
-							<div class="panel-body">
-								<span><h6 style="color: #878787"> 5 de mai de 2017 - PAM Três Lagoas</h6></span>
-								<p style="color: red"><strong> José Melo segue no governo do AM até publicação de acórdão, diz TRE</strong></p>
-								<p style="color: #444">
-								Governador teve cassação mantida pelo TSE. Presidente da ALE-AM assumirá cargo após execução de acórdão. Facebook · Twitter. Por G1 AM. 05/05/2017 12h44 Atualizado há 8 horas. José Melo (Foto: Adneison Severiano/G1 AM)... </p>
-							</div>
-						</div>
-					</a>
-					<a href="#">
-						<div class="panel ">
-							<div class="panel-body">
-								<span><h6 style="color: #ccc"> 5 de mai de 2017 - PAM Três Lagoas</h6></span>
-								<p style="color: red"><strong> José Melo segue no governo do AM até publicação de acórdão, diz TRE</strong></p>
-								<p style="color: #444">
-								Governador teve cassação mantida pelo TSE. Presidente da ALE-AM assumirá cargo após execução de acórdão. Facebook · Twitter. Por G1 AM. 05/05/2017 12h44 Atualizado há 8 horas. José Melo (Foto: Adneison Severiano/G1 AM)... </p>
-							</div>
-						</div>
-					</a>
-					<a href="#">
-						<div class="panel ">
-							<div class="panel-body">
-								<span><h6 style="color: #ccc"> 5 de mai de 2017 - PAM Três Lagoas</h6></span>
-								<p style="color: red"><strong> José Melo segue no governo do AM até publicação de acórdão, diz TRE</strong></p>
-								<p style="color: #444">
-								Governador teve cassação mantida pelo TSE. Presidente da ALE-AM assumirá cargo após execução de acórdão. Facebook · Twitter. Por G1 AM. 05/05/2017 12h44 Atualizado há 8 horas. José Melo (Foto: Adneison Severiano/G1 AM)... </p>
-							</div>
-						</div>
-					</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-3">
-			<div class="col-xs-12">
-				<div class="thumbnail">
-					<img src="img/tumb1.jpg" alt="" class="img-responsive"><br>
-					<div class="caption">
-						<h3>Dia de treinamento</h3>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget </p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12" >
-				<img src="img/face.png" alt="" class="img-responsive" id="facebook"><br>
-			</div>
-			<div class="col-xs-12">
-				<div class="thumbnail">
-					<div class="caption">
-						<h3>Faça Parte</h3>
-						<p>Às empresas interessadas, nossos documentos para Análise:</p>
-						<ul>
-							<li>Estatuto do PAM</li>
-							<li> Termo de Compromisso</li>
-							<li> Ficha de Inscrição</li>
-							<li> Kit de Emergência do PAM Alto Tietê </li>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Home</a></li>
+							<li id="menu-link-dropdown" class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Painel <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Area Restrita</a></li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Institucional <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Sobre</a></li>
+									<li><a href="#">Coordenadores</a></li>
+									<li><a href="#">Integrantes</a></li>
+									<li><a href="#">Atividades</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Fotos</a></li>
+							<li><a href="#">Agenda</a></li>
+							<li><a href="#">Contato</a></li>
 						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+
+		</div>
+		<div class="row" id="corpo">
+			<div class="col-xs-9">
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="carousel-pam" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+								<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<div class="col-xs-6 col-xs-offset-1">
+										<h1 style="color: black;">
+											PAM – 23 Anos
+											<br/>
+										</h1>
+										<h3 style="color:#ebbc11">
+											“Sinergia e Sustentabilidade no Alto Tietê”.
+										</h3>
+									</div>
+									<div class="col-xs-5">
+										<img src="img/img1.4.png" alt="...">
+									</div>
+								</div>
+								<div class="item">
+									<div class="col-xs-6 col-xs-offset-1">
+										<h1 style="color: black;">Excelência
+											<br/>
+										</h1>
+										<h3 style="color:#ebbc11">
+											Nossos treinamentos são ministrados e certificados por órgãos competentes, sob coordenação Operacional do 17º GB. 
+										</h3>
+									</div>
+									<div class="col-xs-5">
+										<img src="img/img2.1.png" alt="...">
+									</div>
+								</div>
+							</div>
+
+							<!-- Controls -->
+							<a class="left carousel-control" href="#carousel-pam" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#carousel-pam" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
+					<div class="col-xs-12">
+						<a href="#">
+							<div class="panel ">
+								<div class="panel-body">
+									<span><h6 style="color: #878787"> 5 de mai de 2017 - PAM Três Lagoas</h6></span>
+									<p style="color: red"><strong> José Melo segue no governo do AM até publicação de acórdão, diz TRE</strong></p>
+									<p style="color: #444">
+										Governador teve cassação mantida pelo TSE. Presidente da ALE-AM assumirá cargo após execução de acórdão. Facebook · Twitter. Por G1 AM. 05/05/2017 12h44 Atualizado há 8 horas. José Melo (Foto: Adneison Severiano/G1 AM)... 
+									</p>
+								</div>
+							</div>
+						</a>
+						<a href="#">
+							<div class="panel ">
+								<div class="panel-body">
+									<span><h6 style="color: #ccc"> 5 de mai de 2017 - PAM Três Lagoas</h6></span>
+									<p style="color: red"><strong> José Melo segue no governo do AM até publicação de acórdão, diz TRE</strong></p>
+									<p style="color: #444">
+										Governador teve cassação mantida pelo TSE. Presidente da ALE-AM assumirá cargo após execução de acórdão. Facebook · Twitter. Por G1 AM. 05/05/2017 12h44 Atualizado há 8 horas. José Melo (Foto: Adneison Severiano/G1 AM)... 
+									</p>
+								</div>
+							</div>
+						</a>
+						<a href="#">
+							<div class="panel ">
+								<div class="panel-body">
+									<span><h6 style="color: #ccc"> 5 de mai de 2017 - PAM Três Lagoas</h6></span>
+									<p style="color: red"><strong> José Melo segue no governo do AM até publicação de acórdão, diz TRE</strong></p>
+									<p style="color: #444">
+										Governador teve cassação mantida pelo TSE. Presidente da ALE-AM assumirá cargo após execução de acórdão. Facebook · Twitter. Por G1 AM. 05/05/2017 12h44 Atualizado há 8 horas. José Melo (Foto: Adneison Severiano/G1 AM)... 
+									</p>											
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-3">
+				<div class="col-xs-12">
+					<a href="#" data-toggle="modal" data-target=".bs-example-modal-lg">
+						<img src="img/curso.jpg" alt="" class="img-responsive"><br>
+					</a>
+					<!-- Large modal -->
+
+					<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+						<div class="modal-dialog modal-lg" role="document">
+							<div class="modal-content">
+							<div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Curso de Bombeiro</h4>
+      </div>					
+      							<div id="cursomid">	
+								<img src="img/curso.jpg" alt="" class="img-responsive">
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12" >
+					<img src="img/face.png" alt="" class="img-responsive" id="facebook"><br>
+				</div>
+				<div class="col-xs-12">
+					<div class="thumbnail">
+						<div class="caption">
+							<h3>Faça Parte</h3>
+							<p>Às empresas interessadas, nossos documentos para Análise:</p>
+							<ul>
+								<li>Estatuto do PAM</li>
+								<li> Termo de Compromisso</li>
+								<li> Ficha de Inscrição</li>
+								<li> Kit de Emergência do PAM Alto Tietê </li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="row" id="rodape">
-		<div class="col-xs-2" id="acesso">
+		<div class="row" id="rodape">
+			<div class="col-xs-2" id="acesso">
 
-			<button type="button" class="btn btn-default" aria-label="Left Align">
-			<span class="glyphicon glyphicon-user" aria-hidden="true"> Login</span>
-			</button>
+				<button type="button" class="btn btn-default" aria-label="Left Align">
+					<span class="glyphicon glyphicon-user" aria-hidden="true"> Login</span>
+				</button>
+			</div>
+			<div class="col-xs-6">
+
+			</div>
+			<div class="col-xs-4" id="end">
+				<address>
+					<strong>PAM Três Lagoas - Plano de Auxílio Mútuo</strong><br>
+					Av. Filinto Muller, 3.300 - Jd. Morumbi<br>
+					Três Lagoas (MS) - CEP 79037-100<br>
+					<abbr title="Phone">Tel:</abbr> (67) - 3509-7800 | 3509-7801
+				</address>
+			</div>
 		</div>
-		<div class="col-xs-6">
-		
-</div>
-		<div class="col-xs-4" id="end">
-			<address>
-  <strong>PAM Três Lagoas - Plano de Auxílio Mútuo</strong><br>
-  Av. Filinto Muller, 3.300 - Jd. Morumbi<br>
-  Três Lagoas (MS) - CEP 79037-100<br>
-  <abbr title="Phone">Tel:</abbr> (67) - 3509-7800 | 3509-7801
-</address>
-		</div>
-	</div>
-	<div class="col-xs-12" id="copy"><h6> Todos os direitos Reservados</h6></div>
+		<div class="col-xs-12" id="copy"><h6> Todos os direitos Reservados</h6></div>
 	</div>
 </body>
 <script src="js/jquery.js"></script>
